0ab405c791da641e75ee09075cb535bcc154df1a
Milo Knowles <mknowles@mit.edu>
2017-12-07 16:22:31 -0500

    Fixes to make things work on image_15

--- StereoReconstruct.m
+++ StereoReconstruct.m
@@ -58,8 +58,6 @@ if options.DATA_FILE
             
         case 1
             % Load the calibration data
-            data_string = 'stereoParams12-5';
-            load([data_string,'.mat']);
             data_string = 'stereo';
             
     end
@@ -105,8 +103,8 @@ while options.RUN_VISION && (options.LIVE_STREAM ||...
                 frame_data.frameLeft = readerLeft.step();
                 frame_data.frameRight = readerRight.step();
             case 1
-                frame_data.frameLeft = imread('Data/left/image_7.jpg');
-                frame_data.frameRight = imread('Data/right/image_7.jpg');
+                frame_data.frameLeft = imread('Data/left/image_15.jpg');
+                frame_data.frameRight = imread('Data/right/image_15.jpg');
         end
     end
     
@@ -126,7 +124,7 @@ while options.RUN_VISION && (options.LIVE_STREAM ||...
     [sliceMap, poly3d] = FitPolytope(frame_data.frameLeftGray,...
                                      point_data.point_cloud,...
                                      point_data.disparityMap,...
-                                     8, 0, 0);
+                                     6, 10, -2);
     PlotPoly3D(poly3d);
     
     % sliceMap is an Nx2 matrix, where column 1 is distance and column 2 is
